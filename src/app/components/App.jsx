498e01b7d727ab608352cb933c2f67422582391d
Beardo <ghislain.roy@mlt.ca>
2017-07-25 08:24:23 -0300

    Added switch

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -9,20 +9,22 @@ import {Category} from "../../category/component";
 //TODO: Add the navigation inside path="/" ex : <Switch> <Route path="/category" render={...} /> <Route path="/profile" render={...} /> </Switch>
 //TODO: Add redirection to login if a user isn't logged in. For now we can use localStorage until we create a server.
 
-export const App = () =>(
+export const App = () => (
     <HashRouter>
         <Switch>
-            <Route path="/" render={()=>
+            <Route path="/" render={() =>
                 <MainView>
-                    <Route path="/home" />
-                    <Route path="/profile" component={Profile}/>
-                    <Route path="/category" component={Category}/>
+                    <Switch>
+                        <Route path="/home"/>
+                        <Route path="/profile" component={Profile}/>
+                        <Route path="/category" component={Category}/>
+                    </Switch>
                 </MainView>
             }/>
 
-            <Route path="/login" />
+            <Route path="/login"/>
 
-            <Route path="/logout" />
+            <Route path="/logout"/>
 
         </Switch>
     </HashRouter>
